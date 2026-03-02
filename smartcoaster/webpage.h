e9a1695a26411ec9ab56f890516b810d0fe5875c
mariusz-tm <s00286100@atu.ie>
2026-03-02 08:36:49 +0000

    Update webpage.h
    Added website layout for phone screens (6.7" display)

--- smartcoaster/webpage.h
+++ smartcoaster/webpage.h
@@ -5,76 +5,123 @@ const char MAIN_page[] = R"rawliteral(
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Smart Coaster Project</title>
-  <style>
-    main{
-      display: flex;
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+  }
+
+  body {
+    background-color: #1C1919;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+    margin-top: 50px;
+    text-align: center;
+  }
+
+  h2 {
+    background-color: #FD5E53;
+    padding: 10px 15px;
+    border-radius: 5px;
+    display: inline-block;
+    font-size: 2em;
+  }
+
+  h3 {
+    font-size: 0.9em;
+    font-weight: normal;
+    color: rgb(165, 159, 159);
+  }
+
+  a {
+    font-size: 1em;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    color: white;
+    text-decoration-color: #FD5E53;
+    text-decoration-thickness: 1px;
+    margin: 0;
+  }
+
+  button {
+    padding: 8px 18px;
+    border: 5px solid #FD5E53;
+    border-radius: 25px;
+    background-color: #f78c84;
+    font-size: 1em;
+    color: white;
+  }
+
+  button:active {
+    border: 5px solid #481916;
+    background-color: #8a2c26;
+    color: gray;
+  }
+
+  .mainBox {
+    width: 31.25%;
+    border: 3px solid #FD5E53;
+    border-radius: 10px;
+    background-color: #161111;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+    min-height: 40vh;
+    text-align: center;
+  }
+
+  /* 6.7" Screen Layout */
+  @media screen and (max-width: 500px) {
+    section {
       flex-direction: column;
-      align-items: center; 
-    }
-    body{
-      background-color: #1C1919;
-      color: white;
-      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      margin-top: 25px;
+      gap: 10px;
     }
 
-    section{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 15px;
-      margin-top: 50px;
+    h2 {
+      font-size: 1.6em;
+      padding: 8px 12px;
     }
-    h2{
-      background-color: #FD5E53;
-      padding: 10px 15px;
-      border-radius: 5px;
-      display: inline-block;
-      font-size: 2em;
-    }
-    h3{
+
+    h3 {
       font-size: 0.8em;
-      font-weight: normal;
-      color: rgb(165, 159, 159)
+      margin: 0 10px;
+      line-height: 1.4em;
     }
+
+    .mainBox {
+      width: 90%;
+      border-width: 2px;
+      min-height: 35vh;
+      padding: 10px 5px;
+    }
+
+    button {
+      font-size: 0.9em;
+      padding: 6px 12px;
+      border-width: 3px;
+    }
+
     a {
-      font-size: 1em;
-      font-family: Verdana, Geneva, Tahoma, sans-serif;
-      color: white;
-      text-decoration-color: #FD5E53;
-      text-decoration-thickness: 1px;
-      margin: 0;
+      font-size: 0.9em;
     }
-    button{
-      padding: 5px;
-      border: 5px solid #FD5E53;
-      border-radius: 25px;
-      background-color: #f78c84;
-      font-size: 1em;
-      color: white;
-    }
-    button:active{
-      border: 5px solid #481916;
-      background-color: #8a2c26;
-      color: gray;
-    }
-    .mainBox{
-      width: 31.25%;
-      height: 150px;
-      border: 3px solid #FD5E53;
-      border-radius: 10px;
-      background-color: #161111;
-      position: relative;
-      box-sizing: border-box;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      text-align: left;
-      margin-top: 10px;
-      min-height: 40vh;
-  </style>
+  }
+</style>
 </head>
 
 <body>
@@ -96,13 +143,11 @@ const char MAIN_page[] = R"rawliteral(
       <p>
         Your Desired Temperature:
         <span id="userTemperature">---</span>
-	<span>C</span>
       </p>
 
       <p>
         Current Temperature of Drink:
         <span id="currentTemperature">---</span>
-	<span>C</span>
       </p>      
       <button  onclick="sendFlag()">Press Here to Turn Off Buzzer</button>
     </section>
